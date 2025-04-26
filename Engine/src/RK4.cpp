e448ade6f1c974170c3d183d5edb6a635add5d79
Daniela Hernandez <yessicadanielahernandez@gmail.com>
2025-04-26 19:24:23 +0100

    feat: rk4 included and adapted with glm library

--- Engine/src/RK4.cpp
+++ Engine/src/RK4.cpp
@@ -1,2 +1,37 @@
 #include "RK4.h"
 
+//----------------------------------------------------------------------------------------------------------------------
+State RK4Integrator::evaluate(const State &_initial, float _t, glm::vec3 force)
+{
+    State output;
+    output.m_position = _initial.m_velocity;
+    output.m_velocity = force;
+    return output;
+}
+//----------------------------------------------------------------------------------------------------------------------
+State RK4Integrator::evaluate(const State &_initial, float _t, float _dt, const State &_d, glm::vec3 force)
+{
+    State state;
+    state.m_position = _initial.m_position + _d.m_position * _dt;
+    state.m_velocity = _initial.m_velocity + _d.m_velocity * _dt;
+    State output;
+    output.m_position = state.m_velocity;
+    output.m_velocity = force;
+    return output;
+}
+//----------------------------------------------------------------------------------------------------------------------
+void RK4Integrator::integrate(float _t, float _dt, glm::vec3 force)
+{
+    // basic RK4 integration we calculate the initial value then two slopes
+    // and combine together see
+    // http://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods for more details
+    State a = evaluate(m_state, _t, force);
+    State b = evaluate(m_state, _t, _dt * 0.5f, a, force);
+    State c = evaluate(m_state, _t, _dt * 0.5f, b, force);
+    State d = evaluate(m_state, _t, _dt, c, force);
+    const glm::vec3 dxdt = 1.0f / 6.0f * (a.m_position + 2.0f * (b.m_position + c.m_position) + d.m_position);
+    const glm::vec3 dvdt = 1.0f / 6.0f * (a.m_velocity + 2.0f * (b.m_velocity + c.m_velocity) + d.m_velocity);
+
+    m_state.m_position = m_state.m_position + dxdt * _dt;
+    m_state.m_velocity = m_state.m_velocity + dvdt * _dt;
+}
--- Engine/src/RK4.h
+++ Engine/src/RK4.h
@@ -1,2 +1,48 @@
 # pragma once
 
+#include <glm/glm.hpp>
+
+// This class was took from the NGL MassSpring DEMO available and I adapted to the context of this project.
+// link to the demo: https://github.com/NCCA/MassSpring.git
+// ------------------------------------------------------------------------------------------------------------
+
+class State
+{
+    public:
+        glm::vec3 m_position;
+        glm::vec3  m_velocity;
+        inline State(glm::vec3 _pos, glm::vec3 _vel) 
+            : m_position(_pos), m_velocity(_vel) {}
+        inline State() {}
+};
+
+/// @brief a simple base class to do RK4 integration this class then needs the motionFunction to be
+/// implemented so we can integrate the values
+
+class RK4Integrator
+{
+    public:
+        RK4Integrator(State _state) 
+            : m_state(_state) {}
+        RK4Integrator() {}
+        
+        State evaluate(const State &_initial, float _t, glm::vec3 force);
+        //----------------------------------------------------------------------------------------------------------------------
+        /// @brief method to evaluate the state given initial value and time step
+        /// @param[in] _initial the initial state
+        /// @param[in] _t the current time step value
+        /// @param[in] _dt the delta time step for integration
+        /// @param[in] _d the derivative for the integration step
+        //----------------------------------------------------------------------------------------------------------------------
+        State evaluate(const State &_initial, float _t, float _dt, const State &_d, glm::vec3 force);
+        //----------------------------------------------------------------------------------------------------------------------
+        /// @brief method to evaluate the state given initial value and time step
+        /// @param[in] _initial the initial state
+        /// @param[in] _t the current time step value
+        /// @param[in] _dt the delta time step for integration
+        //----------------------------------------------------------------------------------------------------------------------
+        void integrate(float _t, float _dt, glm::vec3 force);
+        inline State getState() const { return m_state; }
+    protected:
+        State m_state;
+};
