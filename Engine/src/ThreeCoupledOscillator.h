173dbf632382b90a9c1a619318b8b133ad50e31f
Daniela Hernandez <yessicadanielahernandez@gmail.com>
2025-04-26 21:46:06 +0100

    feat: adding model description

--- Engine/src/ThreeCoupledOscillator.h
+++ Engine/src/ThreeCoupledOscillator.h
@@ -1,2 +1,5 @@
 # pragma once
 
+// This model was taking from the paper called: An analysis of heart rhythm dynamics using a three-coupled oscillator model
+// Authors: Sandra R.F.S.M Gois & Marcelo A. Savi
+
