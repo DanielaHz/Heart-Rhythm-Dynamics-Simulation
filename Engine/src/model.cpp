b8028748a60e51a4b2bcc492c4cc0d240078777a
Daniela Hernandez <yessicadanielahernandez@gmail.com>
2025-05-23 17:32:38 +0100

    feat: loadtetramodel, has extension, and process mesh implementation

--- Engine/src/model.cpp
+++ Engine/src/model.cpp
@@ -1,33 +1,52 @@
 /*
  * MODEL: A model has a mesh, a material, and channels (position, rotation, scale).
  * This class loads(Assimp) and draws models
+ * This class also loads .msh 
  */
 
 #include <string>
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 #include "model.h"
 
 void Model::draw(Shader& shader) {
     // Draw all meshes in the model
-    for (unsigned int i = 0; i < meshes.size(); i++)
+    for (unsigned int i = 0; i < meshes.size(); i++) {
         meshes[i].draw(shader);
+    }
 }
 
-void Model::loadModel(std::string path) {
-    Assimp::Importer import;
-    const aiScene* scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+bool Model::hasExtension(const std::string& path, const std::string& ext)
+{
+	return path.size()>= ext.size() && path.compare(path.size() - ext.size(), ext.size(), ext) == 0;
+}
 
-    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+void Model::loadModel(const std::string& path){
+
+    if (hasExtension(path, ".msh"))
     {
-        cout << "ERROR::ASSIMP::" << import.GetErrorString() << endl;
-        return;
+        loadTetraModel(path);
     }
-    directory = path.substr(0, path.find_last_of('/'));
+    else
+    {
+        Assimp::Importer import;
+        const aiScene* scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
-    processNode(scene->mRootNode, scene);
+        if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+        {
+            cout << "ERROR::ASSIMP::" << import.GetErrorString() << endl;
+            return;
+        }
+        directory = path.substr(0, path.find_last_of('/'));
+
+        processNode(scene->mRootNode, scene);
+        std::cout << "The model have " << meshes.size() << " meshes." << std::endl;
+    }
 }
 
 void Model::processNode(aiNode* node, const aiScene* scene)
@@ -48,14 +67,12 @@ void Model::processNode(aiNode* node, const aiScene* scene)
 Mesh Model::processMesh(aiMesh* mesh, const aiScene* scene)
 {
     std::set<Vertex> uniqueVertices;
-    std::map<std::string, std::vector<Vertex>> heartZones;
     std::map<Vertex, unsigned int> VertexToIndex; 
     std::vector<Vertex> vertices;
     std::vector<unsigned int> indices;
     std::vector<Texture> textures;
 
     //-- DanielaHz refactor --//
-
     for (unsigned int i = 0; i < mesh->mNumVertices; i++)
     {
         Vertex vertex;
@@ -76,7 +93,7 @@ Mesh Model::processMesh(aiMesh* mesh, const aiScene* scene)
         }
     } 
 
-    processMeshZones(vertices, heartZones);
+    std::cout <<"vertices size:" << vertices.size() << std::endl;
 
     // Index processing
     for (unsigned int i = 0; i < mesh->mNumFaces; i++)
@@ -96,40 +113,71 @@ Mesh Model::processMesh(aiMesh* mesh, const aiScene* scene)
         }
     }
     // -- refactor finished -- //
-
-
-    std::cout << "::ZONES SUMMARY::\n";
-    for (const auto& [zone, verts] : heartZones) {
-        std::cout << "Zone: " << zone << " -> " << verts.size() << " vertices\n";
-    }
-    
     return Mesh(vertices, indices, textures); 
-}
+} 
 
+// DanielaHz implementation
+void Model::loadTetraModel(const std::string& path) {
+    gmsh::initialize();
+    gmsh::open(path);
 
-void Model::processMeshZones(std::vector<Vertex> vertices, std::map<std::string, std::vector<Vertex>> &heartZones)
-{
-    float delta = 0.200f;
+    std::vector<std::array<double, 3>> nodePositions;
+    std::vector<std::array<int, 4>> tetrahedra;
 
-    auto isClose = [&](const glm::vec3& a, const glm::vec3& b) {
-        return fabs(a.r - b.r) <= delta &&
-            fabs(a.g - b.g) <= delta &&
-            fabs(a.b - b.b) <= delta;
-    };
+    std::vector<std::size_t> nodeTags;
+    std::vector<double> nodeCoords, parametricCoords;
+    gmsh::model::mesh::getNodes(nodeTags, nodeCoords, parametricCoords);
 
-    glm::vec3 hpcColor = glm::vec3(1.0f, 0.4f, 0.8392f);
-    glm::vec3 avColor  = glm::vec3(0.6039f, 0.251f, 1.0f);
-    glm::vec3 saColor  = glm::vec3(0.2784f, 0.6039f, 1.0f);
+    // Saving nodes position 
+    for (std::size_t i = 0; i < nodeCoords.size(); i += 3) {
+        nodePositions.push_back({ nodeCoords[i], nodeCoords[i+1], nodeCoords[i+2] });
+    }
 
-    for (const auto& vertex : vertices) {
-        if (isClose(vertex.rgb, hpcColor)) {
-            heartZones["hpc"].push_back(vertex);
-        } else if (isClose(vertex.rgb, avColor)) {
-            heartZones["av"].push_back(vertex);
-        } else if (isClose(vertex.rgb, saColor)) {
-            heartZones["sa"].push_back(vertex);
-        } else {
-            heartZones["not"].push_back(vertex);
+    // Nodetag processing
+    std::unordered_map<std::size_t, int> nodeIdToIndex;
+    for (std::size_t i = 0; i < nodeTags.size(); ++i) {
+        nodeIdToIndex[nodeTags[i]] = static_cast<int>(i);
+    }
+
+    // Get tetrahedros
+    std::vector<int> elementTypes;
+    std::vector<std::vector<std::size_t>> elementTags, elementNodeTags;
+    gmsh::model::mesh::getElements(elementTypes, elementTags, elementNodeTags);
+
+    for (std::size_t i = 0; i < elementTypes.size(); ++i) {
+        if (elementTypes[i] == 4) { // tipo 4 = tetraedro
+            const auto& nodes = elementNodeTags[i];
+            for (std::size_t j = 0; j < nodes.size(); j += 4) {
+                tetrahedra.push_back({
+                    nodeIdToIndex[nodes[j]],
+                    nodeIdToIndex[nodes[j+1]],
+                    nodeIdToIndex[nodes[j+2]],
+                    nodeIdToIndex[nodes[j+3]]
+                });
+            }
         }
     }
+
+    gmsh::finalize();
+
+    // generate list of vertices
+    std::vector<Vertex> vertices;
+    for (auto& pos : nodePositions) {
+        Vertex v;
+        v.position = glm::vec3(pos[0], pos[1], pos[2]);
+        v.normal = glm::vec3(0.0f);
+        v.rgb = glm::vec3(1.0f); 
+        vertices.push_back(v);
+    }
+
+    // procesing thetrahedro data
+    std::vector<unsigned int> indices;
+    for (auto& tet : tetrahedra) {
+        indices.push_back(tet[0]);
+        indices.push_back(tet[1]);
+        indices.push_back(tet[2]);
+        indices.push_back(tet[3]);
+    }
+    meshes.clear();
+    meshes.push_back(Mesh(vertices, indices, {}));
 }
--- Engine/src/model.h
+++ Engine/src/model.h
@@ -1,16 +1,16 @@
 #pragma once
 
 #include <string>
-
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-
+#include <gmsh.h>
 #include "mesh.h"
 #include "shader.h"
 #include "gameObject.h"
 #include "set"
 #include "map"
+#include  <memory>
 
 class Model : public GameObject {
 public:
@@ -20,16 +20,17 @@ public:
     ~Model() {}
 
     glm::vec3 color;
-
     void draw(Shader& shader);
 
-    // model data
     vector<Mesh> meshes;
     string directory;
+    std::vector<glm::vec3> nodes;
+    std::vector<std::array<int, 4>> tetrahedra;
 
 private:
-    void loadModel(std::string path);
+    static bool hasExtension(const std::string& path, const std::string& ext);
+    void loadModel(const std::string& path);
     void processNode(aiNode* node, const aiScene* scene);
     Mesh processMesh(aiMesh* mesh, const aiScene* scene);
-    void processMeshZones(std::vector<Vertex> vertices, std::map<std::string,std::vector<Vertex>> &heartZones);
+    void loadTetraModel(const std::string& model);
 };
